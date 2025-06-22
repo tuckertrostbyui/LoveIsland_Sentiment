a12fdcf1f70dc63f39ed380e62c9a954b2b26818
Tucker Trost <167843643+tuckertrostbyui@users.noreply.github.com>
2025-06-21 21:15:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.markdown("<h1 style='text-align: center; color: Black;'>🏝️Love Island US
 st.markdown("<h6 style='text-align: center; color: gray;'>Analysing Reddit Sentiment of Love Island USA Season 7</h6>", unsafe_allow_html=True)
 
 # Load the data
-df = pd.read_csv("data/li_initial.csv")
+df = pd.read_parquet("data/li_initial.csv")
 
 # Convert AirDate to datetime
 df["airdate"] = pd.to_datetime(df["airdate"])
